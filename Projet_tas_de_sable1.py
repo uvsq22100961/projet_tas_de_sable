c8d31b6ecd57a0a1fabde8d57f5b48ee16fc3d11
Thibault ROYERE <thibault.royere@ens.uvsq.fr>
2022-02-08 12:49:30 +0100

    bouton1

--- Projet_tas_de_sable1.py
+++ Projet_tas_de_sable1.py
@@ -12,5 +12,9 @@ import tkinter as tk
 
 racine = tk.Tk()
 canvas = tk.Canvas(racine, height=HEIGHT, width=WIDTH)
-canvas.grid()
+Bouton1 = tk.Button(racine, text="créer la configuration aléatoire")
+
+
+canvas.grid(row=0, culumn=1)
+Bouton1.grid(row=0, column=0)
 canvas.mainloop()
