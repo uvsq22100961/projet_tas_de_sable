fb5d6c91739dda13d700b43bfd5e380ff9ef3447
Thibault ROYERE <thibault.royere@ens.uvsq.fr>
2022-02-08 12:29:42 +0100

    création_canvas

--- Projet_tas_de_sable1.py
+++ Projet_tas_de_sable1.py
@@ -5,6 +5,12 @@
 # Manira MAHAMAT HAGGAR
 # url du dépôt github https://github.com/uvsq22100961/projet_tas_de_sable
 
-print("hello world")
-# camille 
-# bonjour
+HEIGHT = 500
+WIDTH = 500
+
+import tkinter as tk
+
+racine = tk.Tk()
+canvas = tk.Canvas(racine, height=HEIGHT, width=WIDTH)
+canvas.grid()
+canvas.mainloop()
